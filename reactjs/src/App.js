efcfe0933e92d18805b62738ca3eeabcba257de6
Gerarca <gerarca88@gmail.com>
2020-11-05 15:10:56 -0400

    Version 1

---
+++ reactjs/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="dragon.gif" alt="img"/>
+        <h1>
+         ¡¡¡ Hola LuMarketo !!!
+        </h1>                 
+      </header>
+    </div>
+  );
+}
+
+export default App;
